5ed26f9496617a2636cd1c3aa97bd0d225e58616
Maciej Brencz <maciej.brencz@gmail.com>
2013-12-20 20:57:07 +0100

    Clean up emitting expressions

--- rules/complex.js
+++ rules/complex.js
@@ -3,8 +3,8 @@ var COMPLEX_SELECTOR_THRESHOLD = 3;
 function rule(analyzer) {
 	analyzer.setMetric('complexSelectors');
 
-	analyzer.on('selector', function(rule, selector, expr) {
-		if (expr.length > COMPLEX_SELECTOR_THRESHOLD) {
+	analyzer.on('selector', function(rule, selector, expressions) {
+		if (expressions.length > COMPLEX_SELECTOR_THRESHOLD) {
 			analyzer.incrMetric('complexSelectors');
 			analyzer.addOffender('complexSelectors', selector);
 		}
