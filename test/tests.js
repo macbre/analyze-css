8af4caf7c1c15d9177d885a1aeb72a0efc72fd39
Maciej Brencz <maciej.brencz@gmail.com>
2013-12-20 19:35:16 +0100

    Add unit tests

---
+++ test/tests.js
@@ -0,0 +1,70 @@
+var analyzer = require('../'),
+	assert = require('assert'),
+	tests,
+	testSelector;
+
+tests = [
+	// comments
+	{
+		css: '.foo { color: red } /* test */',
+		metrics: {
+			comments: 1,
+			commentsLength: 6
+		}
+	},
+
+	// empty rules
+	{
+		css: '.foo { } .bar { color: red } ',
+		metrics: {
+			emptyRules: 1
+		}
+	},
+
+	// importants
+	{
+		css: '.important { color: red !important }',
+		metrics: {
+			importants: 1
+		}
+	},
+
+	// length
+	{
+		css: '.important { color: red !important }',
+		metrics: {
+			length: 36
+		}
+	},
+
+	// stats
+	{
+		css: '.foo, .bar { color: red; border: blue }',
+		metrics: {
+			rules: 1,
+			selectors: 2,
+			declarations: 2
+		}
+	}
+];
+
+testSelector = function(test) {
+	describe('should return proper metrics', function() {
+		it('for "' + test.css + '"', function(done) {
+			new analyzer(test.css, function(err, res) {
+				var actualMetrics = res.metrics,
+					expectedMetrics = test.metrics;
+
+				Object.keys(expectedMetrics).forEach(function(metric) {
+					assert.equal(expectedMetrics[metric], actualMetrics[metric], metric + ' should equal ' + expectedMetrics[metric] + ' (was ' + actualMetrics[metric]  + ')');
+				});
+
+				done();
+			});
+		});
+	});
+};
+
+describe('Analyzer', function() {
+	tests.forEach(testSelector);
+});
