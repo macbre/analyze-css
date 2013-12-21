8d4410e3a07150f3f17b8dc5e610b96e322e2bfc
Maciej Brencz <maciej.brencz@gmail.com>
2013-12-21 21:00:19 +0100

    Add unit tests

--- test/tests.js
+++ test/tests.js
@@ -79,6 +79,20 @@ tests = [
 		}
 	},
 
+	// outdated vendor prefixes
+	{
+		css: '.foo { -moz-border-radius: 2px }',
+		metrics: {
+			oldPropertyPrefixes: 1
+		}
+	},
+	{
+		css: '.foo { -webkit-animation: none }',
+		metrics: {
+			oldPropertyPrefixes: 0
+		}
+	},
+
 	// qualified selectors
 	{
 		css: '.foo, #bar, header {}',
