c737114d33e7b8ed9f8648ca97307911813e9e1c
Maciej Brencz <maciej.brencz@gmail.com>
2013-12-28 20:53:29 +0100

    bodySelectors: add a rule

---
+++ rules/bodySelectors.js
@@ -0,0 +1,39 @@
+function rule(analyzer) {
+	analyzer.setMetric('redundantBodySelectors');
+
+	analyzer.on('selector', function(rule, selector, expressions) {
+		// check more complex selectors only
+		if (expressions.length < 2) {
+			return;
+		}
+
+		var firstTag = expressions[0].tag,
+			isDescendantCombinator = expressions[1].combinator === '>',
+			isRedundant = false;
+
+		// matches "html > body" fixes
+		if (firstTag === 'html' && isDescendantCombinator && expressions[1].tag === 'body') {
+			isRedundant = false;
+		}
+		// matches "body .foo", but not "body > .bar'
+		else if (firstTag === 'body' && !isDescendantCombinator) {
+			isRedundant = true;
+		}
+		// matches ".foo body > h2"
+		else {
+			expressions.slice(1).forEach(function(expr) {
+				if (expr.tag === 'body') {
+					isRedundant = true;
+				}
+			});
+		}
+
+		if (isRedundant) {
+			analyzer.incrMetric('redundantBodySelectors');
+			analyzer.addOffender('redundantBodySelectors', selector);
+		}
+	});
+}
+
+rule.description = 'Reports redundant body selectors';
+module.exports = rule;
