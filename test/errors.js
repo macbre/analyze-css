10f26bd73bdf82e33ae1e8266166716abd4ce2e1
Maciej Brencz <maciej.brencz@gmail.com>
2013-12-30 18:00:48 +0100

    Test errors handling

---
+++ test/errors.js
@@ -0,0 +1,42 @@
+var analyzer = require('../'),
+	assert = require('assert'),
+	tests;
+
+tests = [
+	{
+		name: 'Empty CSS',
+		css: '',
+		check: /Empty CSS was provided/
+	},
+	{
+		name: 'CSS with whitespaces only',
+		css: '   ',
+		check: /Empty CSS was provided/
+	},
+	{
+		name: 'Non-string value',
+		css: false,
+		check: /css parameter passed is not a string/
+	},
+	{
+		css: 'foo bar',
+		check: /CSS parsing failed/
+	},
+	{
+		css: 'foo bar [abc]',
+		check: /CSS parsing failed/
+	}
+];
+
+describe('Errors handling', function() {
+	tests.forEach(function(test) {
+		describe(test.name || '"' + test.css + '" CSS snippet', function() {
+			it('should raise an error', function(done) {
+				new analyzer(test.css, function(err, res) {
+					assert.equal(test.check.test(err && err.toString()), true, err);
+					done();
+				});
+			});
+		});
+	});
+});
