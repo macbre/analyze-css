3ff72e526d47ad8379c55b5f7471559405bd0c2a
Maciej Brencz <maciej.brencz@gmail.com>
2014-01-02 23:13:45 +0100

    duplicated: handle media queries

--- rules/duplicated.js
+++ rules/duplicated.js
@@ -1,15 +1,34 @@
 'use strict';
 
 var collection = require('../lib/collection'),
+	debug = require('debug')('analyze-css:duplicated'),
 	format = require('util').format;
 
 function rule(analyzer) {
-	var selectors = new collection();
+	var selectors = new collection(),
+		mediaQueryStack = [];
 
 	analyzer.setMetric('duplicatedSelectors');
 
+	// handle nested media queries
+	analyzer.on('media', function(query) {
+		mediaQueryStack.push(query);
+		debug('push: %j', mediaQueryStack);
+	});
+
+	analyzer.on('mediaEnd', function(query) {
+		mediaQueryStack.pop(query);
+		debug('pop: %j', mediaQueryStack);
+	});
+
+	// register each rule's selectors
 	analyzer.on('rule', function(rule) {
-		selectors.push(rule.selectors.join(', '));
+		selectors.push(
+			// @media foo
+			(mediaQueryStack.length > 0 ? '@media ' + mediaQueryStack.join(' @media ') + ' ' : '') +
+			// #foo
+			rule.selectors.join(', ')
+		);
 	});
 
 	analyzer.on('report', function() {
