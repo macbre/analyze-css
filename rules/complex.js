97d6d23de977d6cf62d58cac2998c6ec311c9cc8
Maciej Brencz <maciej.brencz@gmail.com>
2014-05-12 20:11:06 +0200

    complexSelectorsByAttribute: add a metric

--- rules/complex.js
+++ rules/complex.js
@@ -4,11 +4,33 @@ var COMPLEX_SELECTOR_THRESHOLD = 3;
 
 function rule(analyzer) {
 	analyzer.setMetric('complexSelectors');
+	analyzer.setMetric('complexSelectorsByAttribute');
 
+	// #foo .bar ul li a
 	analyzer.on('selector', function(rule, selector, expressions) {
 		if (expressions.length > COMPLEX_SELECTOR_THRESHOLD) {
 			analyzer.incrMetric('complexSelectors');
 			analyzer.addOffender('complexSelectors', selector);
+		}
+	});
+
+	// .foo[type*=bar]
+	// @see http://www.w3.org/TR/css3-selectors/#attribute-selectors
+	analyzer.on('expression', function(selector, expression) {
+		if (typeof expression.attributes !== 'undefined') {
+			switch (expression.attributes[0].operator) {
+				case '=':
+					break;
+
+				case '~=': // contains value in a whitespace-separated list of words
+				case '|=': // starts with value or value-
+				case '^=': // starts with
+				case '$=': // ends with
+				case '*=': // contains
+					analyzer.incrMetric('complexSelectorsByAttribute');
+					analyzer.addOffender('complexSelectorsByAttribute', selector);
+					break;
+			}
 		}
 	});
 }
