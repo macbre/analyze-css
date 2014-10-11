246ef8c860a9398e383ef0cefc672d8860177189
Maciej Brencz <maciej.brencz@gmail.com>
2014-10-11 18:01:35 +0200

    childSelectors: add a rule (#51)

---
+++ rules/childSelectors.js
@@ -0,0 +1,59 @@
+'use strict';
+
+function rule(analyzer) {
+	// definition of redundant child nodes selectors (see #51 for the initial idea):
+	// ul li
+	// ol li
+	// table tr
+	// table th
+	var redundantChildSelectors = {
+		'ul': ['li'],
+		'ol': ['li'],
+		'select': ['option'],
+		'table': ['tr', 'th'], // e.g. table can not be followed by any of tr / th
+	};
+
+	analyzer.setMetric('redundantChildNodesSelectors');
+
+	analyzer.on('selector', function(rule, selector, expressions) {
+		var noExpressions = expressions.length;
+
+		// check more complex selectors only
+		if (noExpressions < 2) {
+			return;
+		}
+
+		// converts "ul#foo > li.test" selector into ['ul', 'li'] list
+		var selectorNodeNames = expressions.map(function(item) {
+			return item.tag;
+		});
+
+		Object.keys(redundantChildSelectors).forEach(function(nodeName) {
+			var nodeIndex = selectorNodeNames.indexOf(nodeName),
+				nextNode,
+				combinator,
+				redundantNodes = redundantChildSelectors[nodeName];
+
+			if ( (nodeIndex > -1) && (nodeIndex < noExpressions - 1) ) {
+				// we've found the possible offender, get the next node in the selector
+				// and compare it against rules in redundantChildSelectors
+				nextNode = selectorNodeNames[nodeIndex + 1];
+
+				if (redundantNodes.indexOf(nextNode) > -1) {
+					combinator = expressions[nodeIndex + 1].combinator;
+
+					// only the following combinator can match:
+					// ul li
+					// ul > li
+					if ( (combinator === ' ') || (combinator === '>') ){
+						analyzer.incrMetric('redundantChildNodesSelectors');
+						analyzer.addOffender('redundantChildNodesSelectors', selector);
+					}
+				}
+			}
+		});
+	});
+}
+
+rule.description = 'Reports redundant child nodes selectors';
+module.exports = rule;
