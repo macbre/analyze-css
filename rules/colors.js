af23bce7365716ed5f21ba97341ba30e086b7760
Maciej Brencz <maciej.brencz@gmail.com>
2014-11-17 23:49:13 +0100

    colors: add a rule (#68)

--- rules/colors.js
+++ rules/colors.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var collection = require('../lib/collection');
+var collection = require('../lib/collection'),
+	debug = require('debug')('analyze-css:colors'),
+	format = require('util').format,
+	onecolor = require('onecolor');
 
 /**
  * Extract CSS colors from given CSS property value
@@ -9,16 +12,50 @@ var regex = /(((rgba?|hsl)\([^\)]+\))|#(\w{3,6}))/g;
 
 function extractColors(value) {
 	var matches = value.match(regex);
-	return matches || [];
+	return matches || false;
 }
 
 function rule(analyzer) {
-	// hash storing unique colors with the counter
+	// store unique colors with the counter
 	var colors = new collection();
 
 	analyzer.setMetric('colors');
 
 	analyzer.on('declaration', function(rule, property, value) {
+		var extractedColors = extractColors(value);
+
+		if (extractedColors === false) {
+			return;
+		}
+
+		debug('%s: %s -> %j', property, value, extractedColors);
+
+		extractedColors.
+			map(function(item) {
+				var color = onecolor(item);
+
+				// handle parsing errors
+				if (color === false) {
+					return false;
+				}
+
+				// return either rgba(0,0,0,0.25) or #000000
+				return (color.alpha() < 1.0) ? color.cssa() : color.hex();
+			}).
+			forEach(function(color) {
+				if (color !== false) {
+					colors.push(color);
+				}
+			});
+	});
+
+	analyzer.on('report', function() {
+		analyzer.setCurrentPosition(undefined);
+
+		colors.sort().forEach(function(color, cnt) {
+			analyzer.incrMetric('colors');
+			analyzer.addOffender('colors', format('%s (%d times)', color, cnt));
+		});
 	});
 }
 
