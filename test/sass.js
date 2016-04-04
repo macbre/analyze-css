9724d1a0a966872b0101d93c959a4f3fcb4bb627
Maciej Brencz <maciej.brencz@gmail.com>
2016-04-04 22:32:41 +0200

    Assert missing "node-sass" is tests

--- test/sass.js
+++ test/sass.js
@@ -6,6 +6,34 @@ var analyzer = require('../'),
 	scss = 'nav {\nul{ color: white }\n}',
 	sass = 'nav\n\tul\n\t\tcolor: white\n';
 
+/**
+ * TODO: install and test node-sass
+ */
+function testSassInstalled(done) {
+	new analyzer(sass, {
+		preprocessor: 'sass'
+	}, function(err, res) {
+		assert.strictEqual(err, null);
+		assert.equal(res.metrics.selectors, 1);
+		done();
+	});
+}
+
+/**
+ * node-sass is not installed by default (see #118)
+ */
+function testSassNotInstalled(done) {
+	try {
+		new analyzer(scss, {
+			preprocessor: 'sass'
+		}, function(err, res) {});
+	} catch(e) {
+		assert.ok(e instanceof Error);
+		assert.equal(e.message, 'Preprocessing failed: Error: Can\'t process SASS/SCSS, please run \'npm install node-sass\'');
+		done();
+	}
+}
+
 describe('SASS preprocessor', function() {
 	it('should be chosen for SCSS files', function() {
 		var preprocessors = new(require('../lib/preprocessors.js'))();
@@ -23,23 +51,7 @@ describe('SASS preprocessor', function() {
 		});
 	});
 
-	it('should generate CSS from SCSS correctly', function(done) {
-		new analyzer(scss, {
-			preprocessor: 'sass'
-		}, function(err, res) {
-			assert.strictEqual(err, null);
-			assert.equal(res.metrics.selectors, 1);
-			done();
-		});
-	});
+	it('should generate CSS from SCSS correctly', testSassNotInstalled);
 
-	it('should generate CSS from SASS correctly', function(done) {
-		new analyzer(sass, {
-			preprocessor: 'sass'
-		}, function(err, res) {
-			assert.strictEqual(err, null);
-			assert.equal(res.metrics.selectors, 1);
-			done();
-		});
-	});
+	it('should generate CSS from SASS correctly', testSassNotInstalled);
 });
