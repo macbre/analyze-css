6dbecec343264f84f592df56f37d0de2d1fb2a1c
macbre <maciej.brencz@gmail.com>
2021-06-11 11:27:58 +0100

    comments.js: a test case for a really (really) long comment

--- test/rules/comments.js
+++ test/rules/comments.js
@@ -12,5 +12,13 @@ exports.tests = [
 			comments: 1,
 			commentsLength: 22
 		}
+	},
+	{
+		css: 'body { padding-bottom: 6em; min-width: 40em }' +
+		'/* really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really long comment */',
+		metrics: {
+			comments: 1,
+			commentsLength: 273
+		}
 	}
 ];
