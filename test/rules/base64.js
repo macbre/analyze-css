dbfeb62f51fe243e633f185068156cf4e5e91546
macbre <maciej.brencz@gmail.com>
2021-06-11 12:20:37 +0100

    Improve code coverage for rules/base64.js

--- test/rules/base64.js
+++ test/rules/base64.js
@@ -3,6 +3,15 @@ exports.tests = [
 		css: '.foo { background-image: url(data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D) }', // blank 1x1 gif
 		metrics: {
 			base64Length: 64
+		},
+		css: '.foo { background-image: url(data:image/gif;base64,' + 'FFFFFF'.repeat(1024) + ') }', // to big base64-encoded asset
+		metrics: {
+			base64Length: 6144
+		},
+		offenders: {
+			base64Length: [
+				".foo { background-image: ... } // base64: 6.00 kB, raw: 4.50 kB"
+			]
 		}
 	}
 ];
