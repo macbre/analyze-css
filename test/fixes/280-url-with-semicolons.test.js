a96b3b60fdbdc3242a5b804f4b90bfa19d4307ad
macbre <maciej.brencz@gmail.com>
2021-06-14 16:25:49 +0100

    test/fixes/280-url-with-semicolons.test.js: one more assertion

--- test/fixes/280-url-with-semicolons.test.js
+++ test/fixes/280-url-with-semicolons.test.js
@@ -15,6 +15,7 @@ body {
         new analyzer(css, (err, res) => {
             assert.ok(err === null, err);
             assert.strictEqual(res.metrics.imports, 1, '@import is detected');
+            assert.strictEqual(res.metrics.selectors, 1, 'body {} is detected');
             assert.deepStrictEqual(res.offenders.imports[0].message, "url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap')");
         });
 	});
