af373b7214e659d59f70be694c2d839fc8dfd3bb
macbre <maciej.brencz@gmail.com>
2021-06-14 16:23:55 +0100

    Handle semicolons in URL import
    Follow the quick fix from https://github.com/reworkcss/css/issues/137 from @dfdeagle47

--- test/fixes/280-url-with-semicolons.test.js
+++ test/fixes/280-url-with-semicolons.test.js
@@ -5,15 +5,17 @@ describe('@import rule with url containing semicolon', () => {
 	it('is properly parsed', () => {
         const analyzer = require('../..'),
             css = `
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap');
 
 body {
 
 }
             `.trim();
 
-        new analyzer(css, (err) => {
+        new analyzer(css, (err, res) => {
             assert.ok(err === null, err);
+            assert.strictEqual(res.metrics.imports, 1, '@import is detected');
+            assert.deepStrictEqual(res.offenders.imports[0].message, "url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap')");
         });
 	});
 });
