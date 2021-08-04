176142d8ff496ba75b86da8c60a3916faec5d484
macbre <maciej.brencz@gmail.com>
2021-08-04 15:05:45 +0100

    Add lib/index.d.ts (resolves #354)

---
+++ lib/index.d.ts
@@ -0,0 +1,70 @@
+/**
+ * This file brings type hinting for index.js file.
+ *
+ * While this package is not written in TypeScript, this file will allow your IDE to provide you with auto-completion.
+ */
+
+declare type MetricsNames =
+  | "base64Length"
+  | "colors"
+  | "comments"
+  | "commentsLength"
+  | "complexSelectors"
+  | "declarations"
+  | "duplicatedProperties"
+  | "duplicatedSelectors"
+  | "emptyRules"
+  | "expressions"
+  | "importants"
+  | "imports"
+  | "length"
+  | "mediaQueries"
+  | "multiClassesSelectors"
+  | "notMinified"
+  | "oldIEFixes"
+  | "oldPropertyPrefixes"
+  | "parsingErrors"
+  | "propertyResets"
+  | "qualifiedSelectors"
+  | "redundantBodySelectors"
+  | "redundantChildNodesSelectors"
+  | "rules"
+  | "selectorLengthAvg"
+  | "selectors"
+  | "selectorsByAttribute"
+  | "selectorsByClass"
+  | "selectorsById"
+  | "selectorsByPseudo"
+  | "selectorsByTag"
+  | "specificityClassAvg"
+  | "specificityClassTotal"
+  | "specificityIdAvg"
+  | "specificityIdTotal"
+  | "specificityTagAvg"
+  | "specificityTagTotal";
+
+/**
+ * Encapsulates a set of metrics
+ */
+declare type Metrics = { [metric in MetricsNames]: number };
+
+/**
+ * Encapsulates a set of offenders
+ */
+declare type Offenders = { [metric in MetricsNames]: Array<Object> };
+
+/**
+ * Encapsulates results from analyze()
+ */
+declare interface Results {
+  generator: string;
+  metrics: Metrics;
+  offenders: Offenders;
+}
+
+/**
+ * The main entry-point to analyze a given css
+ */
+declare function analyze(css: string, options?: object): Promise<Results>;
+
+export = analyze;
