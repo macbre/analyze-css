cc2b7bfa33423d99ec3caefe80398ad0d1baed38
macbre <maciej.brencz@gmail.com>
2021-08-11 16:08:16 +0100

    test/rules.test.js: allow to specifiy which rule to test via TEST_RULE env variable

--- test/rules.test.js
+++ test/rules.test.js
@@ -1,4 +1,10 @@
+/**
+ * You can test a specific rule only by providing TEST_RULE env variable.
+ *
+ * e.g. $ TEST_RULE=stats npx jest test/rules.test.js
+ */
 const { describe, it } = require("@jest/globals");
+const process = require('process');
 
 const analyzer = require('../'),
 	assert = require('assert'),
@@ -7,36 +13,34 @@ const analyzer = require('../'),
 function testCase(test, testId, testName) {
 
 	it(`case #${testId + 1}`, async () => {
-		analyzer(test.css).then(res => {
-			const metricsExpected = test.metrics || {},
-				offendersExpected = test.offenders || {},
-				metricsActual = res && res.metrics,
-				offendersActual = res && res.offenders;
+		const res = await analyzer(test.css);
 
-			function it(name, fn) {
-				// console.debug(name + "\n");
-				fn();
-			}
+		const metricsExpected = test.metrics || {},
+			offendersExpected = test.offenders || {},
+			metricsActual = res && res.metrics,
+			offendersActual = res && res.offenders;
 
-			Object.keys(metricsExpected).forEach(function(metric) {
-				it(
-					'should emit "' + metric + '" metric with a valid value - #' + (testId + 1),
-					() => {
-						assert.strictEqual(metricsActual[metric], metricsExpected[metric], `${testName}: testing ${metric} metric against: ${test.css}`);
-					}
-				);
-			});
+		function it(name, fn) {
+			// console.debug(name + "\n");
+			fn();
+		}
 
-			Object.keys(offendersExpected).forEach(function(metric) {
-				it(
-					'should emit offender for "' + metric + '" metric with a valid value - #' + (testId + 1),
-					() => {
-						assert.deepStrictEqual(offendersActual[metric].map(item => item.message), offendersExpected[metric], `${testName}: testing ${metric} offender against: ${test.css}`);
-					}
-				);
-			});
-		}).catch(e => {
-			console.error(e.message);
+		Object.keys(metricsExpected).forEach(function(metric) {
+			it(
+				'should emit "' + metric + '" metric with a valid value - #' + (testId + 1),
+				() => {
+					assert.strictEqual(metricsActual[metric], metricsExpected[metric], `${testName}: testing ${metric} metric against: ${test.css}`);
+				}
+			);
+		});
+
+		Object.keys(offendersExpected).forEach(function(metric) {
+			it(
+				'should emit offender for "' + metric + '" metric with a valid value - #' + (testId + 1),
+				() => {
+					assert.deepStrictEqual(offendersActual[metric].map(item => item.message), offendersExpected[metric], `${testName}: testing ${metric} offender against: ${test.css}`);
+				}
+			);
 		});
 	});
 }
@@ -46,11 +50,16 @@ function testCase(test, testId, testName) {
  */
 describe('Rules', () => {
 	const files = glob.sync(__dirname + "/rules/*.js"),
-		nameRe = /([^/]+)\.js$/;
+		nameRe = /([^/]+)\.js$/,
+		selectedRule = process.env.TEST_RULE;
 
 	files.forEach(function(file) {
 		var name = file.match(nameRe)[1],
 			testDef = require(file).tests || [];
+
+		if (selectedRule && selectedRule !== name) {
+			return;
+		}
 
 		describe(name, () => {
 			testDef.forEach((testItem, testId) => {
