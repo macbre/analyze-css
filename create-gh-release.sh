9bd86578dfb91d932a944ef0919dda8475ba16c5
macbre <maciej.brencz@gmail.com>
2021-10-13 11:28:16 +0100

    bump-version-patch: run ./create-gh-release.sh helper script

---
+++ create-gh-release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# @see https://cli.github.com/manual/gh_release_create
+
+export VERSION=v$(grep '"version"' package.json | grep -Eo '[0-9\.]+')
+export NOTES=$(git log | grep 'Updating rules/prefixes.json' | head -n 1 | sed "s/    //g")
+
+echo "Creating a GitHub release for tag ${VERSION} (with notes '${NOTES}') ..."
+
+gh release create $VERSION --title 'Updating prefixes data'  --notes "${NOTES}"
