b5097886207a9eede5f7a5948281b6c2726c91ed
macbre <maciej.brencz@gmail.com>
2021-10-15 14:17:41 +0100

    create-gh-release.sh: use npm version + jq combo to extract the current version

--- create-gh-release.sh
+++ create-gh-release.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # @see https://cli.github.com/manual/gh_release_create
 
-export VERSION=v$(grep '"version"' package.json | grep -Eo '[0-9\.]+')
+export VERSION=v$(npm version --json | jq '."analyze-css"' | sed 's/"//g')
 export NOTES=$(git log | grep 'Updating rules/prefixes.json' | head -n 1 | sed "s/    //g")
 
 echo "::notice::Creating a GitHub release for tag ${VERSION} (with notes '${NOTES}') ..."
 
 set -x
-gh release create $VERSION --title 'Updating prefixes data'  --notes "${NOTES}"
-
-echo "::notice::Done"
+gh release create $VERSION --title 'Updating prefixes data'  --notes "${NOTES}" && echo "::notice::Done" || echo "::error::Failed"
